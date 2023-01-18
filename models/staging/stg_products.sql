011303ee893925b208017ab18b933b9d0ccaef59
isashah@kubrickgroup.com <isashah@kubrickgroup.com>
2023-01-18 14:10:18 +0000

    JINJA STUFF

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,4 +1,6 @@
+{% set products_table = 'products' %}
+
 with source as (
-    select * from {{ source('ecommshop', 'products') }}
+    select * from {{ source('ecommshop', products_table) }}
 )
 select * from source
